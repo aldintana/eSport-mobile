c18dbed0eb2da082c5fdf1d5309ffe83b5ab74ad
aldintana <aldin.tanovic@edu.fit.ba>
2022-11-03 15:21:17 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/e_sport_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.e_sport_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: SafeArea(child: Text('Home')),
+    );
+  }
+}
